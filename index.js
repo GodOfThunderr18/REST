ff0c610a64e93f164a08e2f9768e1eb181117d18
Sushanth <sushanthkartikeya18@gmail.com>
2025-07-09 10:06:00 +0530

    added view and edit routes

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.use(express.json());
 
 const { v4: uuidv4 } = require('uuid');
 
+const methodOverride=require('method-override');
+app.use(methodOverride('_method'));
 
 
 app.listen(port,()=>{
@@ -50,5 +52,28 @@ app.post("/posts",(req,res)=>{
     let id=uuidv4();
     let {username,tweet}=req.body;
     posts.push({username,tweet,id});
+    res.redirect("/posts");
+}) 
+
+//view route
+app.get("/posts/:id",(req,res)=>{
+    let {id}=req.params;
+    let post=posts.find((p)=>p.id===id);
+    res.render("show.ejs",{post,id});
+})
+
+
+//update route
+app.get("/posts/:id/edit",(req,res)=>{
+    let {id}=req.params;
+    let post=posts.find((p)=>p.id===id);
+    res.render("edit.ejs",{post});
+})
+
+app.patch("/posts/:id",(req,res)=>{
+    let {id}=req.params;
+    let post=posts.find((p)=>p.id===id);
+    let newContent=req.body.tweet;
+    post.tweet=newContent;
     res.redirect("/posts");
 })
